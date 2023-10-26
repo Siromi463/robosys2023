b150aad31f0e940a4a52f077c2db4f6d6e310155
Siromi463 <kimimi.nyan@gmail.com>
2023-10-26 12:39:46 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,5 +1,17 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Fujioka Kimiyasu kimimi.nyan@gmail.com
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
 
 out=$(seq 5 | ./plus)
+[ "${out}" = 14 ] || ng ${LINENO}
 
-[ "${out}" = 15.0 ]
+[ "$res" = 0 ] && echo OK
+exit $res
