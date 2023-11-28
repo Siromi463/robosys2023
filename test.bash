cde0892e66c9da43606a2689627e565f891cae1d
Siromi463 <kimimi.nyan@gmail.com>
2023-11-28 21:16:11 +0900

    aaaa

--- test.bash
+++ test.bash
@@ -23,8 +23,11 @@ out=$(seq 4 | ./plus)
 標準入力で１００を割っていった最終値：4.166666666666667
 上四つの合計値：128.16666666666666" ] || ng ${LINENO}
 
+out=$(echo A | ./plus)
+[ "${out}" = "入力された文字列の各Unicodeの合計:65" ] || ng ${LINENO}
 
-
+out=$(echo Hello World | ./plus)
+[ "${out}" = "入力された文字列の各Unicodeの合計:1052" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
